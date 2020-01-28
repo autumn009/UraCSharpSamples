8600c8c5debd441149c3ed9a1278ad77356c0984
Akira Kawamata <autumn@piedey.co.jp>
2020-01-28 16:05:10 +0900

    Add ArrayList

---
+++ ArrayList/ArrayList/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+
+class Program
+{
+    static void Main()
+    {
+        ArrayList list = new ArrayList();
+        list.Add(2);
+        list.Add(3);
+        int sum = (int)list[0] + (int)list[1];
+        Console.WriteLine(sum);
+    }
+}
