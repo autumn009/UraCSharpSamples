adc925dc9c8ac04c6a29955e7bec9ea6b9b95074
Akira Kawamata <autumn@piedey.co.jp>
2020-01-31 14:15:44 +0900

    add ArrayWrapperClass

---
+++ ArrayWrapperClass/ArrayWrapperClass/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+
+public class IntCollectionWrapper
+{
+    private ArrayList list = new ArrayList();
+    public void Add(int n)
+    {
+        list.Add(n);
+    }
+    public int[] GetArray()
+    {
+        return (int[])list.ToArray(typeof(int));
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        var x = new IntCollectionWrapper();
+        x.Add(1);
+        x.Add(2);
+        Console.WriteLine(x.GetArray().Sum());
+    }
+}
