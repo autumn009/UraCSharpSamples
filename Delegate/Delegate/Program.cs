db618321e38324fcc996d008e72728d9195b2537
Akira Kawamata <autumn@piedey.co.jp>
2020-01-31 15:44:13 +0900

    add Delegate

---
+++ Delegate/Delegate/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Action a = delegate ()
+        {
+            Console.WriteLine("In Anonymous Method");
+        };
+        Action b = () =>
+        {
+            Console.WriteLine("In Lambda Expression ");
+        };
+        a();
+        b();
+    }
+}
