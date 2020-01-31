22aa9516fe21fecef962389b55f1a96dbac2e6db
Akira Kawamata <autumn@piedey.co.jp>
2020-01-31 14:30:38 +0900

    add LockObject

---
+++ LockObject/LockObject/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+class Program
+{
+    private static object lockobj = new object();
+    private static void subNew()
+    {
+        lock (lockobj)
+        {
+            foreach (var item in Enumerable.Range(0, 10)) Console.Write(item);
+        }
+    }
+
+    private static void subOld()
+    {
+        lock (typeof(Program))
+        {
+            foreach (var item in Enumerable.Range(0, 10)) Console.Write(item);
+        }
+    }
+
+
+    static void Main()
+    {
+        var t1 = Task.Run(subNew);
+        var t2 = Task.Run(subNew);
+        Task.WaitAll(t1, t2);
+        var t3 = Task.Run(subOld);
+        var t4 = Task.Run(subOld);
+        Task.WaitAll(t3, t4);
+    }
+}
