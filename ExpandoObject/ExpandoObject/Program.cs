c7756669e065666bd700a2e87536c32f05032ec1
Akira Kawamata <autumn@piedey.co.jp>
2020-02-01 15:30:53 +0900

    add ExpandoObject

---
+++ ExpandoObject/ExpandoObject/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Dynamic;
+
+class Program
+{
+    static void Main()
+    {
+        dynamic dataStore = new ExpandoObject();
+        dataStore.Alice = 1;
+        dataStore.Betty = 2;
+        Console.WriteLine(dataStore.Alice);
+        Console.WriteLine(dataStore.Betty);
+        Console.WriteLine(dataStore.Cindy);
+    }
+}
