8f713c87b4138419986b83544007ccd8427245fa
Akira Kawamata <autumn@piedey.co.jp>
2020-02-01 14:13:30 +0900

    add WrongName

---
+++ WrongName/WrongName/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    private static bool leotard(string blue, string red)
+    {
+        return blue == "root" && red == "password";
+    }
+
+
+    static void Main(string[] args)
+    {
+        if (leotard("root", "passwor"))
+            Console.WriteLine("Logged in");
+        else
+            Console.WriteLine("Authentication failed");
+    }
+}
